8cb2be75753b165c4623dc31f2a8b318a2255b7c
Dmitry Kurbatov <dmitry.kurbatov@workato.com>
2025-04-17 21:26:15 +0300

    Update main.tf

--- use1/00_base_region/main.tf
+++ use1/00_base_region/main.tf
@@ -1,5 +1,5 @@
 module "base_region" {
-  source = "https://github.com/kandeshvari/tf-modules-a//base_region_wrp?ref=v1.0.0"
+  source = "git::https://github.com/kandeshvari/tf-modules-a.git//base_region_wrp?ref=v1.0.0"
 
   cluster_name = var.cluster_name
 }
